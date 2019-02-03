de9fb52a12a594d6a35afbc5aae23a97827c0906
unknown <sadanalansp@gmail.com>
2019-02-03 13:05:39 +0530

    timer task cancel changes

--- Java8/src/com/timertask/ScheduleTimerTask.java
+++ Java8/src/com/timertask/ScheduleTimerTask.java
@@ -5,11 +5,14 @@ import java.util.Timer;
 
 public class ScheduleTimerTask {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Timer timer = new Timer();
 		Date date = new Date(System.currentTimeMillis());
-		System.out.println("Timer task starts "+ date);
+		System.out.println(" Timer task starts " + date);
 		timer.scheduleAtFixedRate(new ProcessTransactions(), date, 5000);
+		Thread.sleep(15000);
+		timer.cancel();
+		System.out.println(" Timer task is cancelled ");
 	}
 
 }
